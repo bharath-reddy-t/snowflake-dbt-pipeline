62e44eebb33601f9e926fff27487c54cf99c4e4c
BharathKumar.T@cdk.com <BharathKumar.T@cdk.com>
2022-09-02 17:15:56 +0000

    Added dl_parties model

---
+++ models/datalake/dl_parties.sql
@@ -0,0 +1,18 @@
+{{
+    config(
+        materialized='incremental',
+        unique_key='PARTY_ID'
+    )
+}}
+
+with source_cte as (
+    select *
+      from {{ source('poc', 'temp_parties') }}
+)
+select *,
+       current_timestamp() as ingestion_timestamp
+  from source_cte
+
+{% if is_incremental() %}
+    where LAST_UPDATE_DATE > (select max(LAST_UPDATE_DATE) from {{ this }}) 
+{% endif %}
