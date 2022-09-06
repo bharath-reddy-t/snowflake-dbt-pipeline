69d6f9304b3d52f7fd8a4c0b9ba959b5c35f83ab
Bharath Kumar <BharathKumar.T@cdk.com>
2022-09-06 07:53:59 +0000

    Update dl_parties

--- models/datalake/dl_parties.sql
+++ models/datalake/dl_parties.sql
@@ -7,9 +7,14 @@
 
 with source_cte as (
     select *
-      from {{ source('poc', 'landing_hz_parties_v2') }}
+      from {{ source('poc', 'temp_parties') }}
 )
 select *,
+       case
+         when YEAR_ESTABLISHED > 2000 then '21st Century'
+         when YEAR_ESTABLISHED <= 2000 then '20th Century'
+         when YEAR_ESTABLISHED is NULL then 'Established year not found'
+       end as Established_Century,
        current_timestamp() as ingestion_timestamp,
        current_user() as dl_last_updated_by
   from source_cte
