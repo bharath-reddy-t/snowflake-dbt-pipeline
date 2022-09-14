c1fe96a7fe4e3f55e1fc79d171390fc125febb70
Jayakrishna Nagam <Nagam.Jayakrishna@cdk.com>
2022-09-14 08:45:39 +0000

    month_name  is added and cases for day_of_the_week is eliminated

--- models/consumption/dim_date.sql
+++ models/consumption/dim_date.sql
@@ -9,25 +9,19 @@ final as (
            row_number() over(order by date) as date_key,
            day(my_date) as day_number,
            month(my_date) as month_number,
+           {{ dbt_date.month_name("my_date", short=false)}}  as month_name,
            year(my_date) as year,
-           dayofweek(my_date) as day_ofweek,
-           case 
-             when day_ofweek = 0 then 'Sunday'
-             when day_ofweek = 1 then 'Monday'
-             when day_ofweek = 2 then 'Tuesday' 
-             when day_ofweek = 3 then 'Wednesday'
-             when day_ofweek = 4 then 'Thursday'
-             when day_ofweek = 5 then 'Friday'
-             when day_ofweek = 6 then 'Saturday'
-            end as day_of_week,
+           {{ dbt_date.day_name("my_date", short=false) }} as day_of_week,
            quarter(my_date) as quarter
       from date_cte
 )
 
+ 
 select date_key,
        date,
        day_number,
        month_number,
+       month_name,
        year,
        day_of_week,
        quarter
