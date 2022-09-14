d4478dd3f10e701d8d594f98c98f4bd98853a963
Bharath Kumar <BharathKumar.T@cdk.com>
2022-09-14 09:54:05 +0000

    Updates

--- models/datalake/dl_parties.sql
+++ models/datalake/dl_parties.sql
@@ -4,10 +4,12 @@
         unique_key='PARTY_ID'
     )
 }}
+
 with source_cte as (
     select *
       from {{ source('poc', 'landing_hz_parties_v2') }}
 )
+
 select *,
        case
          when YEAR_ESTABLISHED > 2000 then '21st Century'
@@ -17,6 +19,7 @@ select *,
        current_timestamp() as ingestion_timestamp,
        current_user() as dl_last_updated_by
   from source_cte
+
 {% if is_incremental() %}
     where LAST_UPDATE_DATE > (select max(LAST_UPDATE_DATE) from {{ this }}) 
        or PARTY_ID not in (select PARTY_ID from {{ this }})
