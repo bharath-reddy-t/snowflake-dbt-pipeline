031ea7893315f257b0d02b7ba8f9d2a785a2d669
Jayakrishna Nagam <Nagam.Jayakrishna@cdk.com>
2022-09-21 09:07:11 +0000

    removed 20 to 42 and 66

--- models/consumption/fact/fact_opportunity.sql
+++ models/consumption/fact/fact_opportunity.sql
@@ -17,29 +17,7 @@ opp as(
 ),
 
 fact_opportunity as(
-<<<<<<< HEAD
-    select
-            id,
-            a.Account_key as Account_key,
-            d.date_key as closed_key,
-            cd.date_key as created_key,
-            amount,
-            expectedrevenue,
-            Probability,
-            DEAL_STATE_ID__C,
-            CB_ORACLE_ACCOUNT_NUMBER__C,
-            MIGRATION_ID__C,
-            QC_TOTAL__C,
-            dl_lastupdateddate,
-            dl_lastupdatedby
-       from opp o
-       left join date d on
-                 d.date=o.closedate 
-       left join date cd on 
-                 cd.date=o.createddate
-       left join accounts a on 
-                 a.acc_id=o.accountid 
-=======
+
     select id as opportunity_id,
            a.Account_key as Account_key,
            d.date_key as closedate_key,
@@ -63,7 +41,7 @@ fact_opportunity as(
       left 
       join accounts a 
         on a.account_id = o.accountid 
->>>>>>> 5779cfa34e78392fcd4cabc07c6fd83a580f2a01
+
 )
 
 select *
