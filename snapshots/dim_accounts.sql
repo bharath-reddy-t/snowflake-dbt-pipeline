0971788542288014b10b2f68d30c540af90d4930
Jayakrishna Nagam <Nagam.Jayakrishna@cdk.com>
2022-09-21 05:54:43 +0000

    please commit done with fact_opportunity

--- snapshots/dim_accounts.sql
+++ snapshots/dim_accounts.sql
@@ -6,12 +6,8 @@
             unique_key = 'ID',
             strategy = 'timestamp',
             updated_at = 'dl_last_update_date',
-<<<<<<< HEAD
-            invalidate_hard_deletes = True,
-             transient = False
-=======
             invalidate_hard_deletes = True
->>>>>>> 1c25cdd83197ebe52200fd3c3cd97446cd209659
+
         )
     }}
 
