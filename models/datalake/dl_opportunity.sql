0771ac3916be84aecaf3cf255bba657fd8fef08e
Jayakrishna Nagam <Nagam.Jayakrishna@cdk.com>
2022-09-21 08:29:14 +0000

    refresh

--- models/datalake/dl_opportunity.sql
+++ models/datalake/dl_opportunity.sql
@@ -6,7 +6,7 @@
 }}
  
 with source_cte as (
-<<<<<<< HEAD
+
     select  cast(CLOSEDATE AS DATE) as CLOSEDATE,
             cast(CREATEDDATE as datetime) as CREATEDDATE,
             cast(LASTMODIFIEDDATE as datetime) as LASTMODIFIEDDATE,
@@ -14,12 +14,7 @@ with source_cte as (
           except=["CLOSEDATE",
                   "CREATEDDATE",
                   "LASTMODIFIEDDATE"]) }}
-=======
-    select cast(CLOSEDATE AS DATE) as CLOSEDATE,
-           cast(CREATEDDATE as datetime) as CREATEDDATE,
-           cast(LASTMODIFIEDDATE as datetime) as LASTMODIFIEDDATE,
-           {{ dbt_utils.star(from=source('poc', 'raw_sf_opportunity') , except=["CLOSEDATE","CREATEDDATE","LASTMODIFIEDDATE"]) }}
->>>>>>> 5779cfa34e78392fcd4cabc07c6fd83a580f2a01
+
       from {{ source('poc', 'raw_sf_opportunity') }}
 )
  
