774d2566a5dcbb50d9c1fe58872eda147ea67504
Bharath Kumar <BharathKumar.T@cdk.com>
2022-09-21 08:50:42 +0000

    Update dim_accounts

--- snapshots/dim_accounts.sql
+++ snapshots/dim_accounts.sql
@@ -3,7 +3,7 @@
     {{
         config(
             target_schema = 'dim',
-            unique_key = 'ID',
+            unique_key = 'account_id',
             strategy = 'timestamp',
             updated_at = 'dl_last_update_date',
             invalidate_hard_deletes = True
@@ -11,8 +11,9 @@
         )
     }}
 
-    select *,
-           row_number() over(order by ID) as Account_key
+    select ID as account_id,
+           row_number() over(order by account_id) as Account_key,
+           {{ dbt_utils.star(from=ref('dl_accounts') , except=['ID','OWNERID','CREATEDBYID','LASTMODIFIEDBYID']) }}
       from {{ ref('dl_accounts') }}
 
 {% endsnapshot %}
