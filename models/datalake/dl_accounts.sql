827c0b976f88922f03a2bc8e1e84d56199be5cef
Bharath Kumar <BharathKumar.T@cdk.com>
2022-09-28 07:15:34 +0000

    Adjust Spacing

--- models/datalake/dl_accounts.sql
+++ models/datalake/dl_accounts.sql
@@ -9,34 +9,34 @@
 with source_cte as (
 
     select {{ dbt_utils.star(from=source('poc', 'raw_sf_account'), 
-              except=["CREATEDDATE","LASTMODIFIEDDATE","LastViewedDate"] ) 
+              except=["CREATEDDATE","LASTMODIFIEDDATE","LastViewedDate"]) 
            }},
-
            try_cast(CREATEDDATE as datetime) as CREATEDDATE,
            try_cast(LASTMODIFIEDDATE as datetime) as LASTMODIFIEDDATE,
            try_cast(LastViewedDate as datetime) as LastViewedDate
-    from {{ source('poc', 'raw_sf_account') }}
+      from {{ source('poc', 'raw_sf_account') }}
 ),
 
 cte as(
-    select * ,
-        ROW_NUMBER() OVER (PARTITION BY id order by id,LASTMODIFIEDDATE desc ) as rownumber,
-        current_timestamp() as dl_last_update_date,
-        current_user() as dl_last_updated_by 
-    from source_cte 
+    select *,
+           ROW_NUMBER() OVER (PARTITION BY id order by id,LASTMODIFIEDDATE desc ) as rownumber,
+           current_timestamp() as dl_last_update_date,
+           current_user() as dl_last_updated_by 
+      from source_cte 
  )
+
 {% if not is_incremental()  %}
 
-    select *  from cte  where rownumber =1
+    select * from cte 
+     where rownumber = 1
 
 {% endif %}
 
 {% if is_incremental() %}
 
-    select *  from cte
-    where 
-       (LASTMODIFIEDDATE > (select max(LASTMODIFIEDDATE) from {{ this }}) 
-       or ID not in (select ID from {{ this }}) ) and 
-       rownumber =1
+    select * from cte
+     where (LASTMODIFIEDDATE > (select max(LASTMODIFIEDDATE) from {{ this }}) 
+        or ID not in (select ID from {{ this }}) ) 
+       and rownumber = 1
 
 {% endif %}
--- models/datalake/dl_opportunity.sql
+++ models/datalake/dl_opportunity.sql
@@ -6,36 +6,36 @@
 }}
  
 with source_cte as (
-
-    select  {{ dbt_utils.star(from=source('poc', 'raw_sf_opportunity') ,
-         except=["CLOSEDATE",
-                  "CREATEDDATE",
-                  "LASTMODIFIEDDATE"]) }},
-         try_cast(CLOSEDATE AS DATE) as CLOSEDATE,
-         try_cast(CREATEDDATE as datetime) as CREATEDDATE,
-         try_cast(LASTMODIFIEDDATE as datetime) as LASTMODIFIEDDATE
-    from {{ source('poc', 'raw_sf_opportunity') }}
+    select {{ dbt_utils.star(from=source('poc', 'raw_sf_opportunity'),
+              except=["CLOSEDATE", "CREATEDDATE", "LASTMODIFIEDDATE"]) 
+           }},
+           try_cast(CLOSEDATE AS DATE) as CLOSEDATE,
+           try_cast(CREATEDDATE as datetime) as CREATEDDATE,
+           try_cast(LASTMODIFIEDDATE as datetime) as LASTMODIFIEDDATE
+      from {{ source('poc', 'raw_sf_opportunity') }}
 ),
 
 cte as(
-    select * ,
-        ROW_NUMBER() OVER (PARTITION BY id order by id,LASTMODIFIEDDATE desc ) as rownumber,
-        current_timestamp() as dl_lastupdateddate,
-        current_user() as dl_lastupdatedby 
-  from source_cte 
+    select *,
+           ROW_NUMBER() OVER (PARTITION BY id order by id,LASTMODIFIEDDATE desc ) as rownumber,
+           current_timestamp() as dl_lastupdateddate,
+           current_user() as dl_lastupdatedby 
+      from source_cte 
  )
 
 {% if not is_incremental()  %}
 
-    select *  from cte  where rownumber =1
+    select * from cte 
+     where rownumber = 1
 
 {% endif %}
  
 {% if is_incremental() %}
+
     select *  from cte
-        where  (LASTMODIFIEDDATE > (select max(LASTMODIFIEDDATE) from {{ this }}) 
-        or ID not in (select ID from {{ this }}) ) and 
-        rownumber =1
+     where (LASTMODIFIEDDATE > (select max(LASTMODIFIEDDATE) from {{ this }}) 
+        or ID not in (select ID from {{ this }}) ) 
+       and rownumber = 1
  
 {% endif %}
  
