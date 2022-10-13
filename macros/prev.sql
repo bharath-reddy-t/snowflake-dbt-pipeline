1e295e40eb665193cfa0ffb2d0b1657ff29e4387
Bharath Kumar <BharathKumar.T@cdk.com>
2022-10-13 08:06:01 +0000

    Update lead_lag

--- macros/prev.sql
+++ macros/prev.sql
@@ -1,4 +1,4 @@
 {% macro prev(column_name) %}
-     LAG({{column_name}}, 1, NULL) OVER(
-       ORDER BY created_date ASC) 
+     lag({{column_name}}) over(partition by deal_id
+       order by created_date asc) 
 {% endmacro %}
--- models/datalake/lead_lag.sql
+++ models/datalake/lead_lag.sql
@@ -1,20 +1,22 @@
 select id, deal_id,
         (case 
-            when ((select count(*) from {{ source('poc2','demo') }}) = 1
-             and old_value is null and new_value is not null)
-            then 'Cancelled'
+            -- when old_value is null and new_value is not null 
+            --  and lag(deal_id) over(partition by deal_id order by created_date) is null
+            --  and lead(deal_id) over(partition by deal_id order by created_date) is null
+            -- then 'Cancelled'
             when old_value is null and deal_id = {{ next('deal_id') }} 
-            Then {{ next('old_value') }} 
-            when old_value is null and deal_id != {{ next('deal_id') }} and deal_id != {{ prev('deal_id') }}
-            Then new_value 
+            then {{ next('old_value') }} 
+            when old_value is null and {{ next('deal_id') }} is null and {{ prev('deal_id') }} is null
+            then new_value 
             else old_value 
         end) as old_value,
         (case 
-            when ((select count(*) from {{ source('poc2','demo') }}) = 1
-             and new_value is null and old_value is not null)
+            when new_value is null and old_value is not null
+             and lag(deal_id) over(partition by deal_id order by created_date) is null
+             and lead(deal_id) over(partition by deal_id order by created_date) is null
             then null
             when new_value is null and deal_id = {{ prev('deal_id') }} 
-            Then {{ prev('old_value') }}  
+            then {{ prev('old_value') }}  
             else new_value 
         end) as new_value,
         status,
@@ -22,6 +24,8 @@ select id, deal_id,
         created_date as start_date,
         (case
             when deal_id != {{ prev('deal_id') }} then current_timestamp()
-            else {{ prev('created_date') }} 
+            else {{ next('created_date') }} 
         end) as end_date
   from {{source('poc2','demo')}}
+ order 
+    by created_date
