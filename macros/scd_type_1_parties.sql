434b433645006b5788cefb573f7c65855bb0db11
Bharath Kumar <BharathKumar.T@cdk.com>
2022-10-17 07:17:23 +0000

    Update macros

--- macros/scd_type_1_parties.sql
+++ macros/scd_type_1_parties.sql
@@ -1,16 +1,16 @@
-{% macro scd_type1() %}
+{% macro scd_type1(party_name, party_type, customer_key, source_table) %}
     
     merge into "ODIN_DEV"."DIM"."PARTIES_SCD_TEST" M
          using "ODIN_DEV"."LANDING_SFDC"."TEMP_PARTIES" C
             on M.PARTY_ID = C.PARTY_ID
           when matched
-           and M.PARTY_NAME <> C.PARTY_NAME
+           and M.{{party_name}} <> C.PARTY_NAME
            and M.DBT_VALID_TO is NULL
-            or M.PARTY_TYPE <> C.PARTY_TYPE
-            or M.CUSTOMER_KEY <> C.CUSTOMER_KEY
+            or M.{{party_type}} <> C.PARTY_TYPE
+            or M.{{customer_key}} <> C.CUSTOMER_KEY
           then update
-           set M.PARTY_NAME = C.PARTY_NAME,
-               M.PARTY_TYPE = C.PARTY_TYPE,
-               M.CUSTOMER_KEY = C.CUSTOMER_KEY
+           set M.{{party_name}} = C.PARTY_NAME,
+               M.{{party_type}} = C.PARTY_TYPE,
+               M.{{customer_key}} = C.CUSTOMER_KEY
 
 {% endmacro %}
